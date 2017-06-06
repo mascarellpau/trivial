d7d2b16095570400873c634e9143db8abcea1469
pau mascarell <pau mascarell>
2017-06-06 20:53:34 +0200

    first commit

---
+++ js/trivial.js
@@ -0,0 +1,64 @@
+function changeColor(x)
+{
+    if(x.style.background=="rgb(250, 0, 0)")
+    {
+        x.style.opacity="0";
+        x.style.background="#fff";
+    }else{
+        x.style.background="rgb(250, 0, 0)";
+        x.style.opacity="1";
+    }
+    return false;
+}
+
+function changeColorRosa(x)
+{
+    if(x.style.background=="rgb(255, 0, 137)")
+    {
+        x.style.opacity="0";
+        x.style.background="#fff";
+    }else{
+        x.style.background="rgb(255, 0, 137)";
+        x.style.opacity="1";
+    }
+    return false;
+}
+
+function changeColorVerd(x)
+{
+    if(x.style.background=="rgb(0, 255, 0)")
+    {
+        x.style.opacity="0";
+        x.style.background="#fff";
+    }else{
+        x.style.background="rgb(0, 255, 0)";
+        x.style.opacity="1";
+    }
+    return false;
+}
+
+function changeColorBlau(x)
+{
+    if(x.style.background=="rgb(0, 0, 255)")
+    {
+        x.style.opacity="0";
+        x.style.background="#fff";
+    }else{
+        x.style.background="rgb(0, 0, 255)";
+        x.style.opacity="1";
+    }
+    return false;
+}
+
+function changeColorGroc(x)
+{
+    if(x.style.background=="rgb(255, 255, 0)")
+    {
+        x.style.opacity="0";
+        x.style.background="#fff";
+    }else{
+        x.style.background="rgb(255, 255, 0)";
+        x.style.opacity="1";
+    }
+    return false;
+}
